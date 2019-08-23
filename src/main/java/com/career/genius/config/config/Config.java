6b4acf23e8416bc0622a098ab519fd0bdad82fa2
Simon <tgs4432@163.com>
2019-08-23 19:33:08 +0800

    Update

--- src/main/java/com/career/genius/config/config/Config.java
+++ src/main/java/com/career/genius/config/config/Config.java
@@ -9,18 +9,22 @@ package com.career.genius.config.config;
  **/
 public class Config {
 
+    /**
+     * 微信公众号/服务号APPID
+     */
     public static String WX_APP_ID = "wx5f6a7488cc0f5a36";
-
-    public static String WX_APP_SECRET = "f8fae63b9f958bb0ecdad6a97dfb1076";
+    public static String WX_APP_SECRET = "c5f6b5d2d38d0e65191c1b1747867df8";
 
     public static String WX_SHARE_URL = "http://vcard.mjgenius.com/wechat/share.html";
 
-    //微信公众平台APPID
+    /**
+     * 微信开放平台APPID
+     */
     public static String WX_OPEN_APP_ID = "wx84f3cce00d3828e4";
-
-    //微信公众平台secret
     public static String WX_OPEN_SECRET = "wx5f6a7488cc0f5a36";
 
-
+    /**
+     * 当前域名
+     */
     public static String CURRENT_DOMAIN = "m.yunmaidianzi.com";
 }
